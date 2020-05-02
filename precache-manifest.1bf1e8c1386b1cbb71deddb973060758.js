d8faab8ea766b68cf166b64e2da8666517387ba8
boly1604 <cheicknouhoun@gmail.com>
2020-05-02 23:30:39 +0000

    Updates

---
+++ precache-manifest.1bf1e8c1386b1cbb71deddb973060758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817adbb12d9f9b2b050b4ab7f3a6e4ef",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "f9d854c70c0c137eb5b9",
+    "url": "/simple-todo-app/static/css/main.da38cb5f.chunk.css"
+  },
+  {
+    "revision": "0adb4576dda19546b8b0",
+    "url": "/simple-todo-app/static/js/2.8558d8e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.8558d8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d854c70c0c137eb5b9",
+    "url": "/simple-todo-app/static/js/main.1fadc52b.chunk.js"
+  },
+  {
+    "revision": "28ce10bd24386f3991d5",
+    "url": "/simple-todo-app/static/js/runtime-main.db2fd63c.js"
+  }
+]);
